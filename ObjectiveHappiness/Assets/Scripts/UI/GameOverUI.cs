4e9903fa682bbe848cc3cf0be71cd2af202ed214
Thomas B <thomas.barlier.auge@gmail.com>
2026-02-28 10:01:06 +0100

    GameOverUI comments

--- ObjectiveHappiness/Assets/Scripts/UI/GameOverUI.cs
+++ ObjectiveHappiness/Assets/Scripts/UI/GameOverUI.cs
@@ -13,6 +13,7 @@ public class GameOverUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Not display some UI menus
         buildManager.SetActive(true);
         buildMenu.SetActive(false);
         resourcesManager.SetActive(true);
@@ -22,6 +23,9 @@ public class GameOverUI : MonoBehaviour
 
     public void GameOverVerification()
     {
+        // Verify if the player is in a gameover situation
+        // Game Over when there is less than 2 villagers
+        // Display the gameover video
         if (ResourceManager.Instance.residents <= 2)
         {
             buildManager.SetActive(false);
@@ -33,6 +37,9 @@ public class GameOverUI : MonoBehaviour
 
     public void WinVerification()
     {
+        // Verify if the player is in a victory situation
+        // Victory when prosperity reach 100 or highter
+        // Display the victory video
         if ( ResourceManager.Instance.prosperity >= 100)
         {
             buildManager.SetActive(false);
